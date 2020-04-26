57be546bebe602f69b721f443422999a0e50b83c
Efstathios Oikonomou <eusoikonomou@users.noreply.github.com>
2020-04-26 14:33:56 +0300

    Passes webpack-dev-server options to webpack.dev.js configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,10 @@ const path = require('path');
 const baseConfig = require('./webpack.config');
 
 module.exports = Object.assign(baseConfig, {
+  mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 9000,
+    open: true,
   },
-  devtool: 'inline-source-map',
 });
